e51b738fc0bef09fcedf2274b5da5edeab340acc
cyril.vella <c.vella@vminnovation.fr>
2023-08-26 22:18:22 +0200

    uploader2

--- src/app/view/theme/song/add-song/add-song.component.ts
+++ src/app/view/theme/song/add-song/add-song.component.ts
@@ -16,32 +16,30 @@ export class AddSongComponent implements OnInit {
     musicForm: FormGroup;
     subcategory: any = [];
     files: any = [];
+    progress: { [key: string]: number } = {};
+    downloadUrls: { [key: string]: string } = {};
     picturebackground: any;
-    urlProgress: number = 0;
-    chanson_wavProgress: number = 0;
-    creatoke_wavProgress: number = 0;
-    creatokeProgress: number = 0;
-    imageProgress: number = 0;
-    constructor(private uploadService: UploadService, private fb: FormBuilder, private categoryService: CategoryService, private songService: SongService, private storage: AngularFireStorage) {
+    constructor(private fb: FormBuilder, private categoryService: CategoryService, private songService: SongService, private storage: AngularFireStorage) {
 
         this.musicForm = this.fb.group({
             title: [''],
             artist: [''],
             lyrics: [''],
             description: [''],
-            price_base: [''],
-            price_premium: [''],
+            price_base_creatoke: [''],
+            price_premium_creatoke: [''],
+            price_base_chanson: [''],
+            price_premium_chanson: [''],
             image: [''],
             category: [''],
             subcategory: [''],
             created_at: [''],
             youtubeURL: [''],
             spotifyURL: [''],
-            full_creatoke: [''],
-            mp3: [''],
-            wav: [''],
+            creatoke: [''],
+            chanson_wav: [''],
+            creatoke_wav: [''],
             url: [''],
-            full_music: [''],
         });
     }
 
@@ -49,7 +47,7 @@ export class AddSongComponent implements OnInit {
         const file = event.target.files[0];
         if (file) {
             this.startUpload(file, fileType);
-        }else{
+        } else {
             console.log(':(')
         }
     }
@@ -59,48 +57,63 @@ export class AddSongComponent implements OnInit {
         this.categoryService.getBackgroundImg().then(r => { this.picturebackground = r.data[0]?.picture });
     }
 
-    addMusic(): void {
-        const currentDate = new Date();
-        const random = Math.floor((Math.random() * 100000) + 1);
-        const day = String(currentDate.getDate()).padStart(2, '0');
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-        const year = currentDate.getFullYear();
-        const formattedDate = `${day}-${month}-${year}`;
-        for (const key in this.musicForm.value) {
-            if (this.musicForm.value.hasOwnProperty(key)) {
-                if (this.musicForm.value[key] === null || this.musicForm.value[key] === undefined || this.musicForm.value[key] === '') {
-                    if (this.musicForm.value['image'] == null) {
-                        this.musicForm.value['image'] = "https://placehold.co/600x400";
-                    } else {
-                        this.musicForm.value[key] = 'vide';
-                    } // Replace with the desired value
-                }
-            }
-        }
-        this.songService.createSong(this.musicForm.value).catch((success) => console.log(success))
-    }
 
     ngOnInit(): void {
         this.categoryService.getSubCategory().then((data) => { this.subcategory = data.data; });
         this.getBackground();
     }
 
-    startUpload(file: File, fileType: string) {
-        const filePath = `uploads/${file.name}`;
-        const task: AngularFireUploadTask = this.storage.upload(filePath, file);
-        const progressProperty = fileType + 'Progress';
-        this[progressProperty] = 0;
+    addMusic(): void {
+        for (const key in this.musicForm.value) {
+            if (this.musicForm.value.hasOwnProperty(key)) {
+                if (
+                    this.musicForm.value[key] === null ||
+                    this.musicForm.value[key] === undefined ||
+                    this.musicForm.value[key] === ''
+                ) {
+                    if (key === 'image') {
+                        this.musicForm.value['image'] = 'https://placehold.co/600x400';
+                    } else {
+                        this.musicForm.value[key] = 'vide';
+                    }
+                    if (key === 'title') {
+                        this.musicForm.value[key].toUpperCase();
+                    }
+                }
+            }
+        }
 
+        this.musicForm.value['creatoke_wav'] = this.downloadUrls['creatoke_wav'];
+        this.musicForm.value['image'] = this.downloadUrls['image'];
+        this.musicForm.value['creatoke'] = this.downloadUrls['creatoke'];
+        this.musicForm.value['url'] = this.downloadUrls['url'];
+
+        this.songService.createSong(this.musicForm.value).catch((success) => console.log(success));
+    }
+
+    startUpload(file: File, fileType: string): void {
+        const filePath = `uploads/${file.name}`;
+        const fileRef = this.storage.ref(filePath);
+
+        const task: AngularFireUploadTask = this.storage.upload(filePath, file);
+        this.progress[fileType] = 0;
         //@ts-ignore
         task.percentageChanges().subscribe((progress: number) => {
-            this[progressProperty] = progress.toFixed(2);
+            //@ts-ignore
+            this.progress[fileType] = progress.toFixed(2);
         });
 
         task.snapshotChanges().pipe(
             finalize(() => {
-                // Handle completion and download URL
-                // Update corresponding property based on fileType
+                fileRef.getDownloadURL().subscribe((url: string) => {
+                    this.handleDownloadURL(url, fileType);
+                });
             })
         ).subscribe();
     }
+
+    handleDownloadURL(url: string, fileType: string): void {
+        this.downloadUrls[fileType] = url;
+    }
 }
+
